a7d79cca45cf3af9df3b3c4bc459c7d07585577d
queenbee-31 <lakshmigallamaheshwari@gmail.com>
2026-05-20 12:22:58 +0000

    Add initial Backend HTML page

---
+++ Backend.java
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Backend Page</title>
+</head>
+<body>
+    <h1>Welcome to the Backend</h1>
+    <p>This is the backend interface of the project.</p>
+
+    <a href="frontend.html">Go to Frontend</a>
+</body>
+</html>
